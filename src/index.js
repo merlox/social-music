bf54aa0de0fcdd31ca8f2fffbf8a5b96d98471fe
merlox <merunasgrincalaitis@gmail.com>
2019-02-12 11:05:19 +0100

    Setup account working with smart contract

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import web3 from 'web3'
+import myWEB3 from 'web3'
 import './index.css'
+import ABI from './SocialMusic.json'
 
 class Main extends React.Component {
     constructor() {
         super()
+
+        window.myWeb3 = new myWEB3(myWEB3.givenProvider)
+        this.state = {
+            isFormHidden: true
+        }
+
+        this.setContractInstance()
+    }
+
+    async getAccount() {
+        return (await myWeb3.eth.getAccounts())[0]
+    }
+
+    async setContractInstance() {
+        const contractAddress = '0x0217ED41bC271a712f91477c305957Da44f91068'
+        const abi = ABI.abi
+        const contractInstance = new myWeb3.eth.Contract(abi, contractAddress, {
+            from: await this.getAccount(),
+            gasPrice: 2e9
+        })
+        await this.setState({contractInstance: contractInstance})
+    }
+
+    async setupAccount(name, age, status) {
+        await this.state.contractInstance.methods.setup(this.fillBytes32WithSpaces(name), age, status).send({from: '0x2f6ccd575FA71e2912a31b65F7aFF45C8bf91155'})
+    }
+
+    fillBytes32WithSpaces(name) {
+        let nameHex = myWeb3.utils.toHex(name)
+        for(let i = nameHex.length; i < 66; i++) {
+            nameHex = nameHex + '0'
+        }
+        return nameHex
     }
 
     render() {
@@ -14,10 +48,24 @@ class Main extends React.Component {
                 <h1>Welcome to Decentralized Social Music!</h1>
                 <p>Setup your account, start adding musical recommendations for your friends and follow people that may interest you</p>
                 <div className="buttons-container">
-                    <button>Setup Account</button>
+                    <button onClick={() => {
+                        if(this.state.isFormHidden) this.setState({isFormHidden: false})
+                        else this.setState({isFormHidden: true})
+                    }}>Setup Account</button>
                     <button>Add Music</button>
                     <button>Follow People</button>
                 </div>
+
+                <Form
+                    className={this.state.isFormHidden ? 'hidden' : ''}
+                    cancel={() => {
+                        this.setState({isFormHidden: true})
+                    }}
+                    setupAccount={(name, age, status) => {
+                        this.setupAccount(name, age, status)
+                    }}
+                />
+
                 <h3>Latest musical recommendations from people using the dApp</h3>
                 <div ref="general-recommendations">
                     <Recommendation
@@ -63,6 +111,32 @@ class Recommendation extends React.Component {
                 <div className="recommendation-address">{this.props.address}</div>
                 <div className="recommendation-song">{this.props.song}</div>
             </div>
+        )
+    }
+}
+
+class Form extends React.Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <form className={this.props.className}>
+                <input className="form-input" type="text" ref="form-name" placeholder="Your name" />
+                <input className="form-input" type="number" ref="form-age" placeholder="Your age" />
+                <textarea className="form-input form-textarea" ref="form-state" placeholder="Your state, a description about yourself"></textarea>
+                <div>
+                    <button onClick={event => {
+                        event.preventDefault()
+                        this.props.cancel()
+                    }} className="cancel-button">Cancel</button>
+                    <button onClick={event => {
+                        event.preventDefault()
+                        this.props.setupAccount(this.refs['form-name'].value, this.refs['form-age'].value, this.refs['form-state'].value)
+                    }}>Submit</button>
+                </div>
+            </form>
         )
     }
 }
