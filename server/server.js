63f6e06b2102471bf76cf3aa7d114fc7b5016b90
merlox <merunasgrincalaitis@gmail.com>
2019-09-01 16:30:18 +0200

    Added yargs and npm start

--- server/server.js
+++ server/server.js
@@ -2,15 +2,27 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
-const port = 9000
 const distFolder = path.join(__dirname, '../public', 'dist')
+const yargs = require('yargs')
+const argv = yargs.option('port', {
+    alias: 'p',
+    description: 'Set the port to run this server on',
+    type: 'number',
+}).help().alias('help', 'h').argv
+if(!argv.port) {
+    console.log('Error, you need to pass the port you want to run this application on with npm start -- -p 8001')
+    process.exit(0)
+}
+const port = argv.port
 
 app.use(distFolder, express.static(distFolder))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-   console.log(`${req.method} Request to ${req.originalUrl}`)
-   next()
+app.use('*', (req, res, next) => {
+	// Logger
+	let time = new Date()
+	console.log(`${req.method} to ${req.originalUrl} at ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+	next()
 })
 app.get('*/bundle.js', (req, res) => {
    res.sendFile(path.join(distFolder, 'bundle.js'))
