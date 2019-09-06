bc830cbe01f78d1d0759d66b9bf0a0dc4a34743f
merlox <merunasgrincalaitis@gmail.com>
2019-09-06 22:44:11 +0200

    Renamed build to bundle

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('*', (req, res, next) => {
 	next()
 })
 
-app.get('*/bundle.js', (req, res) => {
+app.get('bundle.js', (req, res) => {
     res.sendFile(path.join(distFolder, 'bundle.js'))
 })
 
